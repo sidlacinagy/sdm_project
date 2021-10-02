a5ae392a686a5c6447fa717c711f4153c36de21e
Nagy László Endre <nlaciendre@gmail.com>
2021-10-02 16:11:15 +0200

    Implemented postmapping with HTML-form data

--- src/main/java/com/example/user_management_system/registration/Request.java
+++ src/main/java/com/example/user_management_system/registration/Request.java
@@ -2,8 +2,10 @@ package com.example.user_management_system.registration;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class Request {
+public class Request implements Serializable {
 
     private final String email;
     private final String firstName;
