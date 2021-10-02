7bf09487cb4d471d4e7cfdcc9da4c45e9ea5c735
Muszkitosz <sz.ziza@gmail.com>
2021-10-02 19:15:41 +0200

    Implemented custom login and registration page. Created an animation for switching between login and registration page

---
+++ src/main/java/com/example/user_management_system/ui/LoginController.java
@@ -0,0 +1,18 @@
+package com.example.user_management_system.ui;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/home")
+    public String viewLoginPage() {
+            return "login";
+        }
+
+
+}
