88bebbb7e668a40e9b19fc441bbc501b36eaf44a
Muszkitosz <sz.ziza@gmail.com>
2021-10-03 14:54:02 +0200

    Added custom login page redirection

--- src/main/java/com/example/user_management_system/security/SecurityConfig.java
+++ src/main/java/com/example/user_management_system/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin().and()
+                .formLogin().defaultSuccessUrl("/profile_home", true).and()
                 .csrf().disable();
     }
 
