484f48fdf0b1cbb6bff04d8193c2d496f77a7a09
Nagy László Endre <nlaciendre@gmail.com>
2021-11-23 22:40:50 +0100

    fixed bugs + updated movie page

--- src/main/frontend/src/pages/movie/MoviePage.js
+++ src/main/frontend/src/pages/movie/MoviePage.js
@@ -1,15 +1,27 @@
 import {loadCredits, loadImages, loadMovie, loadRecommendations, loadVideos} from "../../api/apicalls";
 import React, {useEffect, useState} from "react";
 import {Helmet} from "react-helmet";
+import {useHistory} from "react-router-dom";
+import not_found from "../searchresult/not_found.png";
+import reactElementToJSXString from 'react-element-to-jsx-string';
+
 
 export function MoviePage(props) {
 
     const [movieInfo, setMovieInfo] = useState({});
-    const [cast, setCast] = useState({});
-    const [directors, setDirectors] = useState({});
+    const [cast, setCast] = useState([]);
+    const [directors, setDirectors] = useState();
     const [images, setImages] = useState({});
-    const [videos, setVideos] = useState({});
-    const [recommendations, setRecommendations] = useState({});
+    const [videos, setVideos] = useState("");
+    const [recommendations, setRecommendations] = useState([]);
+    const[videoTag,setVideoTag]= useState();
+    const history = useHistory();
+    const[currentCastPage,setCurrentCastPage]= useState();
+    const[currentCast,setCurrentCast]= useState([]);
+    const[currentRecomPage,setCurrentRecomPage]= useState();
+    const[currentRecom,setCurrentRecom]= useState([]);
+
+
 
     useEffect(() => {
         loadMovie((window.location.href).split('?')[1]).then((response) => {
@@ -17,11 +29,20 @@ export function MoviePage(props) {
         })
 
         loadCredits((window.location.href).split('?')[1]).then((response) => {
+            setDirectors(response.data.crew.filter(crew => crew.job == "Director").map((crew) => (
+                <li>
+                    {crew.name}
+                </li>
+            )))
+
             setCast(response.data.cast.map((actor) => (
                 <li>
-                    {actor.name}
+                    {actor.name} played {actor.character}
+                    <img alt="pic" src={actor.profile_path === null ? not_found : ("https://image.tmdb.org/t/p/w500/" + actor.profile_path)}
+                         width="100px" />
                 </li>
-            )));
+            )))
+
         })
 
         loadImages((window.location.href).split('?')[1]).then((response) => {
@@ -29,13 +50,86 @@ export function MoviePage(props) {
         })
 
         loadVideos((window.location.href).split('?')[1]).then((response) => {
-            setVideos(response.data);
+            setVideos("https://www.youtube.com/embed/"+response.data.results.find(element => element.type==="Trailer").key);
+            setVideoTag(<span><td>Video:</td>
+            <td> <iframe width="420" height="315" allow="fullscreen;"
+                         src={videos}>
+            </iframe> </td></span>)
         })
 
         loadRecommendations({movie_id:(window.location.href).split('?')[1], page: "1"}).then((response) => {
-            setRecommendations(response.data);
+            setRecommendations(response.data.results.map((movie) => (
+
+                <li id={movie.id} onClick={handleMovieClick}>
+                    <img alt="pic" src={movie.poster_path === null ? not_found : ("https://image.tmdb.org/t/p/original" + movie.poster_path)}
+                         width="100px" id={movie.id} onClick={handleMovieClick}/>
+                    <div className="movie_li_div" id={movie.id} onClick={handleMovieClick}>
+                        <span className="movie_title" id={movie.id} onClick={handleMovieClick}>{movie.title}</span>
+                        <span className="movie_release_date" id={movie.id}
+                              onClick={handleMovieClick}> {movie.release_date === null ? "" : movie.release_date.substring(0, 4)}</span>
+                        <br/>
+                        <span id={movie.id}
+                              onClick={handleMovieClick}>Original title: {movie.original_title === null ? "" : movie.original_title}</span>
+                        <br/>
+                        <span id={movie.id} onClick={handleMovieClick}>Ratings</span>
+                    </div>
+                </li>
+
+            )));
         })
+
     }, []);
+
+    useEffect(() => {setCurrentCast(Array.of(cast[(currentCastPage * 5)],
+        cast[(1+(currentCastPage * 5))],
+        cast[(2+(currentCastPage * 5))],
+        cast[(3+(currentCastPage * 5))],
+        cast[(4+(currentCastPage * 5))]))
+            } ,[currentCastPage]);
+
+    useEffect(() => {setCurrentRecom(Array.of(recommendations[(currentRecomPage * 5)],
+        recommendations[(1+(currentRecomPage * 5))],
+        recommendations[(2+(currentRecomPage * 5))],
+        recommendations[(3+(currentRecomPage * 5))],
+        recommendations[(4+(currentRecomPage * 5))]))
+    } ,[currentRecomPage]);
+
+    useEffect(
+        () => {
+            let timer1 = setTimeout(() => setCurrentCastPage(0), 500);
+            let timer2 = setTimeout(() => setCurrentRecomPage(0), 500);
+            return () => {
+                clearTimeout(timer1);
+                clearTimeout(timer2);
+            };
+        },
+        []
+    );
+
+    function nextCastPage(event) {
+        setCurrentCastPage(currentCastPage+1);
+    }
+
+    function previousCastPage(event){
+        setCurrentCastPage(currentCastPage-1);
+    }
+
+    function nextRecomPage(event) {
+        setCurrentRecomPage(currentRecomPage+1);
+    }
+
+    function previousRecomPage(event){
+        setCurrentRecomPage(currentRecomPage-1);
+    }
+
+    function handleMovieClick(event) {
+        window.location.href ="/movie?" + event.target.id;
+    }
+
+
+    function handleMovieClick(event) {
+        window.location.href ="/movie?" + event.target.id;
+    }
 
     function handleSwitchToDashboard() {
         props.history.push("/dashboard");
@@ -89,26 +183,52 @@ export function MoviePage(props) {
                             </tr>
                             <tr>
                                 <td>Cast:</td>
-                                <td><ul>
-                                    {cast}
-                                </ul></td>
+                                <td>
+                                    <div className="dashboard-button" id="back"
+                                         style={currentCastPage<1 ? {display: 'none'} : {}}>
+                                        <div onClick={() => previousCastPage()}>Back</div>
+                                </div>
+                                    <ul id="currentCast">
+                                        {currentCast}
+                                    </ul>
+
+                                    <div className="dashboard-button" id="next"
+                                         style={(cast.length)<=(currentCastPage+1)*5 ? {display: 'none'} : {}}>
+                                    <div onClick={() => nextCastPage()}>Next</div>
+                                </div>
+                                </td>
                             </tr>
                             <tr>
                                 <td>Director:</td>
-                                <td><span>{movieInfo.revenue}</span></td>
+                                <td><ul>
+                                    {directors}
+                                </ul></td>
                             </tr>
-                            <tr>
-                                <td>Video:</td>
-                                <td><span>{movieInfo.revenue}</span></td>
-                            </tr>
+                                {videos !== "" ? <tr><td>Video:</td>
+            <td> <iframe width="420" height="315" allow="fullscreen;"
+                         src={videos}>
+            </iframe> </td></tr> : ""}
                             <tr>
                                 <td>Recommendations:</td>
-                                <td><span>{movieInfo.revenue}</span></td>
+                                <td>
+                                    <div className="dashboard-button" id="back"
+                                         style={currentRecomPage<1 ? {display: 'none'} : {}}>
+                                        <div onClick={() => previousRecomPage()}>Back</div>
+                                    </div>
+                                    <ul id="currentRecom">
+                                        {currentRecom}
+                                    </ul>
+
+                                    <div className="dashboard-button" id="next"
+                                         style={(recommendations.length)<=(currentRecomPage+1)*5 ? {display: 'none'} : {}}>
+                                        <div onClick={() => nextRecomPage()}>Next</div>
+                                    </div>
+                                </td>
                             </tr>
                         </table>
                     </div>
                     <div className="dashboard-button">
-                        <div onClick={handleSwitchToDashboard}>Dashboard</div>
+                        <div onClick={() => history.goBack()}>Back</div>
                     </div>
                 </div>
             </div>
